7742154fe3c1bc9d1c7a19c1db9d04b134e12c45
SC-AndreyMolchanov <amch@sitecore.net>
2019-07-26 15:21:16 +0300

    commit initial code

---
+++ src/Sitecore.Support.331669/Commerce/CustomModels/PageEvents/Converters/ConvertPageEventDataToVisitedProductDetailsPageEvent.cs
@@ -0,0 +1,69 @@
+﻿using Sitecore.Analytics.Model;
+using Sitecore.Analytics.XConnect.DataAccess.Pipelines.ConvertToXConnectEventPipeline;
+using Newtonsoft.Json;
+using Sitecore.Commerce.AnalyticsData;
+using Sitecore.Commerce.CustomModels;
+using System.Collections.Generic;
+using System.Globalization;
+using Sitecore.XConnect;
+using Sitecore.Commerce.CustomModels.PageEvents;
+
+namespace Sitecore.Support.Commerce.CustomModels.PageEvents.Converters
+{
+    public class ConvertPageEventDataToVisitedProductDetailsPageEvent : ConverterBase
+    {
+        public ConvertPageEventDataToVisitedProductDetailsPageEvent() : base()
+        {
+        }
+
+        public void PopulateEventFromJson(string pageEventJson, Event @event)
+        {
+            pageEventJson = this.CleanupMongoMigrationJson(pageEventJson);
+
+            dynamic json = JsonConvert.DeserializeObject(pageEventJson);
+
+            var pageEventData = new PageEventData();
+
+            var data = AnalyticsDataInitializerFactory.Create<VisitedProductDetailsPageAnalyticsData>();
+
+            Dictionary<string, object> customValuesDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(json.CustomValues.ToString());
+
+            data.Deserialize(customValuesDict);
+
+            this.PopulatePageEventFromBsonCustomValues(data, pageEventData);
+
+            this.TranslateEvent(pageEventData, @event as VisitedProductDetailsPageEvent);
+        }
+
+        protected override bool CanProcessPageEventData(PageEventData pageEventData)
+        {
+            return pageEventData.PageEventDefinitionId == VisitedProductDetailsPageEvent.ID;
+        }
+
+        protected override Event CreateEvent(PageEventData pageEventData)
+        {
+            var pageEvent = new VisitedProductDetailsPageEvent(pageEventData.DateTime);
+
+            this.TranslateEvent(pageEventData, pageEvent);
+
+            return pageEvent;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1011:ConsiderPassingBaseTypesAsParameters")]
+        protected void TranslateEvent(PageEventData pageEventData, VisitedProductDetailsPageEvent pageEvent)
+        {
+
+            pageEvent.ShopName = pageEventData.CustomValues[Sitecore.Commerce.Constants.KnownPageEventDataNames.ShopName] as string;
+            pageEvent.ProductId = pageEventData.CustomValues[Sitecore.Commerce.Constants.KnownPageEventDataNames.ProductId] as string;
+            pageEvent.ProductName = pageEventData.CustomValues[Sitecore.Commerce.Constants.KnownPageEventDataNames.ProductName] as string;
+            pageEvent.ParentCategoryId = pageEventData.CustomValues[Sitecore.Commerce.Constants.KnownPageEventDataNames.ParentCategoryId] as string;
+            pageEvent.ParentCategoryName = pageEventData.CustomValues[Sitecore.Commerce.Constants.KnownPageEventDataNames.ParentCategoryName] as string;
+            pageEvent.CurrencyCode = pageEventData.CustomValues[Sitecore.Commerce.Constants.KnownPageEventDataNames.Currency] as string;
+            pageEvent.Amount = System.Convert.ToDecimal(pageEventData.CustomValues[Sitecore.Commerce.Constants.KnownPageEventDataNames.Amount], CultureInfo.InvariantCulture);
+
+
+
+
+        }
+    }
+}
---
+++ src/Sitecore.Support.331669/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.331669")]
+[assembly: AssemblyProduct("Sitecore.Support.331669")]
+[assembly: ComVisible(false)]
